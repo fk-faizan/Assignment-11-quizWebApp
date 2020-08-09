4fb7f0f1158f76b87cf4e20a4a90f325649c2dc6
fk-faizan <faizankhn.fk97@gmail.com>
2020-08-09 15:35:26 +0500

    Faizan Khan | WM3333 | faizankhn.fk97@gmail.com | 03368253427

--- js/app.js
+++ js/app.js
@@ -1,91 +1,150 @@
-window.onload = function() {
-    show(0);
+window.onload = function () {
+  show(0);
+  timer();
+  let user_name = sessionStorage.getItem("name");
+  document.querySelector("span.name").innerHTML = user_name;
+  // console.log(user_name)
 }
 
 // Questions 
 let questions = [
-    {
-      id: 1,
-      question: "What is the full form of RAM ?",
-      answer: "Random Access Memory",
-      options: [
-        "Random Access Memory",
-        "Randomely Access Memory",
-        "Run Aceapt Memory",
-        "None of these"
-      ]
-    },
-    {
-      id: 2,
-      question: "What is the full form of CPU?",
-      answer: "Central Processing Unit",
-      options: [
-        "Central Program Unit",
-        "Central Processing Unit",
-        "Central Preload Unit",
-        "None of these"
-      ]
-    },
-    {
-      id: 3,
-      question: "What is the full form of E-mail",
-      answer: "Electronic Mail",
-      options: [
-        "Electronic Mail",
-        "Electric Mail",
-        "Engine Mail",
-        "None of these"
-      ]
-    }
-  ];
+  {
+    id: 1,
+    question: "What is the full form of RAM ?",
+    answer: "Random Access Memory",
+    options: [
+      "Random Access Memory",
+      "Randomely Access Memory",
+      "Run Aceapt Memory",
+      "None of these"
+    ]
+  },
+  {
+    id: 2,
+    question: "What is the full form of CPU?",
+    answer: "Central Processing Unit",
+    options: [
+      "Central Program Unit",
+      "Central Processing Unit",
+      "Central Preload Unit",
+      "None of these"
+    ]
+  },
+  {
+    id: 3,
+    question: "What is the full form of E-mail",
+    answer: "Electronic Mail",
+    options: [
+      "Electronic Mail",
+      "Electric Mail",
+      "Engine Mail",
+      "None of these"
+    ]
+  }
+];
 
 
 function submitForm(e) {
-    e.preventDefault();
-    let name = document.forms["main-form"]["name"].value;
+  e.preventDefault();
+  let name = document.forms["main-form"]["name"].value;
 
-    if(name !== "") {
-        sessionStorage.setItem("name", name);
-        location.href = "./quiz.html";
-    } else {
-        alert("Enter Your Name To Start Quiz !")
-    }
+  if (name !== "") {
+    sessionStorage.setItem("name", name);
+    location.href = "./quiz.html";
+  } else {
+    alert("Enter Your Name To Start Quiz !")
+  }
 
-    // console.log(name)
+  // console.log(name)
 }
 
 let question_count = 0;
+let points = 0;
 
 function next() {
-    question_count++;
-    show(question_count);
+
+  if (question_count == questions.length - 1) {
+    location.href = "end.html";
+    show(0);
+    return;
+  }
+
+  let user_answer = document.querySelector("li.option.active").innerHTML;
+  // check if the answer is right or wrong
+  if (user_answer == questions[question_count].answer) {
+    points += 10;
+    sessionStorage.setItem("points", points);
+    sessionStorage.setItem("tltQues", questions.length);
+
+    console.log(points)
+  }
+  question_count++;
+  show(question_count);
 }
 
 function show(count) {
-    let question = document.getElementById("questions");
+  let question = document.getElementById("questions");
+  let quesDetail = document.getElementById("questions-detail");
 
-    question.innerHTML = '';
+  let ef = count + 1;
+  let eef = questions.length;
 
-    let h2 = document.createElement("h2");
-    let h2Text = document.createTextNode(questions[count].question);
-    h2.appendChild(h2Text);
-    question.appendChild(h2);
+  question.innerHTML = '';
+  quesDetail.innerHTML = ef+ ' of ' + eef + ' Question';
 
-    let ul = document.createElement("ul");
-    ul.setAttribute('class', 'option-group');
-    question.appendChild(ul);
+  let h2 = document.createElement("h2");
+  let h2Text = document.createTextNode(questions[count].question);
+  h2.appendChild(h2Text);
+  question.appendChild(h2);
 
-    for(var i = 0; i < questions[count].options.length; i++) {
-        let li = document.createElement("li");
-        let liText = document.createTextNode(questions[count].options[i]);
-        li.setAttribute('class', 'option');
-        li.appendChild(liText);
-        ul.appendChild(li);
+  let ul = document.createElement("ul");
+  ul.setAttribute('class', 'option-group');
+  question.appendChild(ul);
+
+  for (let i = 0; i < questions[count].options.length; i++) {
+    let li = document.createElement("li");
+    let liText = document.createTextNode(questions[count].options[i]);
+    li.setAttribute('class', 'option');
+    li.appendChild(liText);
+    ul.appendChild(li);
+  }
+
+  toggleActive();
+}
+
+function toggleActive() {
+  let option = document.querySelectorAll("li.option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].onclick = function () {
+      for (let i = 0; i < option.length; i++) {
+        if (option[i].classList.contains("active")) {
+          option[i].classList.remove("active");
+        }
+      }
+      option[i].classList.add("active");
+    };
+  }
+}
+
+function timer() {
+  let dt = new Date(new Date().setTime(0));
+  let ctime = dt.getTime();
+  let seconds = Math.floor((ctime % (1000 * 60)) / 1000);
+  let minutes = Math.floor((ctime % (1000 * 60 * 60)) / (1000 * 60));
+  console.log(seconds, minutes);
+  let time = 0;
+  let mytime = setInterval(function () {
+    time++;
+
+    if (seconds < 35) {
+      seconds++;
+    } else if (seconds == 35) {
+      ++question_count;
+      show(question_count);
+      seconds = 0
+      seconds++
     }
-
-    // console.log(question)
-
-    // question.innerHTML = "<h2>" + questions[count].question + "</h2>";
-    // print li with loop
-
+    let formatted_sec = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    document.querySelector("span.time").innerHTML = `${formatted_sec}`;
+  }, 1000);
 }
